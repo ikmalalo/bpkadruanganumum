ebc0ab4a713950f4605b6b442dc81a565d774502
mpasd4.py <ikmalaliashari@gmail.com>
2026-03-17 15:09:06 +0800

    fix: Update database configuration for production environment

--- ManajemenRuanganUmum/backend/src/config/db.js
+++ ManajemenRuanganUmum/backend/src/config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'mysql.railway.internal',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'bpkadumum',
+  password: process.env.DB_PASSWORD || 'wwMTekVOtrGrRvLsjvCGsohiPNITeHdj',
+  database: process.env.DB_NAME || 'railway',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
