5702f92166d27224704769c489682d65fe7d45ba
mpasd4.py <ikmalaliashari@gmail.com>
2026-03-17 13:33:49 +0800

    config vite.config.tsx

--- ManajemenRuanganUmum/frontend/vite.config.ts
+++ ManajemenRuanganUmum/frontend/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ⬅️ tambahin ini
-})
+
+  base: "./", // 🔥 WAJIB untuk deploy
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  server: {
+    allowedHosts: ["terina-undappled-stacy.ngrok-free.dev"],
+
+    // 🔥 ini cuma buat LOCAL
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+      "/uploads": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
+  },
+});
