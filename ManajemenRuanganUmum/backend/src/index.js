aeb6699b377a8a260db6d35a31b82a9d92c0f847
mpasd4.py <ikmalaliashari@gmail.com>
2026-03-25 08:53:14 +0800

    perbaiki ulang koneksi

--- ManajemenRuanganUmum/backend/src/index.js
+++ ManajemenRuanganUmum/backend/src/index.js
@@ -6,7 +6,7 @@ const agendaRoutes = require('./routes/agendaRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-const PORT = 9000;
+const PORT = process.env.PORT || 59489;
 
 app.use(cors());
 app.use(express.json());
