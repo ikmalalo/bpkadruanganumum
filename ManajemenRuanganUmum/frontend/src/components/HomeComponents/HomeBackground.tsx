4abd6f2c53ed64719781100255958b1a5bd7dc31
mpasd4.py <ikmalaliashari@gmail.com>
2026-03-26 16:11:50 +0800

    fixing colour vanta js NET

--- ManajemenRuanganUmum/frontend/src/components/HomeComponents/HomeBackground.tsx
+++ ManajemenRuanganUmum/frontend/src/components/HomeComponents/HomeBackground.tsx
@@ -7,7 +7,7 @@ if (THREE.ColorManagement) {
 }
 
 // @ts-ignore
-import NET from "vanta/dist/vanta.net.min";
+import NET from "../lib/vanta.net.js";
 
 export default function HomeBackground() {
   const [vantaEffect, setVantaEffect] = useState<any>(null);
