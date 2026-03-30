891329214f4446df5103c935d8c4d2b2d0ff9eec
mpasd4.py <ikmalaliashari@gmail.com>
2026-03-30 12:10:38 +0800

    lower opacity table horizontal

--- ManajemenRuanganUmum/frontend/src/pages/PreviewHorizontal.tsx
+++ ManajemenRuanganUmum/frontend/src/pages/PreviewHorizontal.tsx
@@ -208,7 +208,7 @@ export default function PreviewHorizontal() {
           </div>
         </div>
 
-        <div className="bg-white rounded-[1.2rem] shadow-xl overflow-hidden border border-gray-100 w-full flex-1 flex flex-col relative">
+        <div className="bg-white/70 backdrop-blur-md rounded-[1.2rem] shadow-xl overflow-hidden border border-white/20 w-full flex-1 flex flex-col relative">
           <div 
             key={currentPage} 
             className="flex-1 flex flex-col w-full"
